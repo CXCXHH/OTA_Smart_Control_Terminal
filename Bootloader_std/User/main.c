fadaf959f796d45e00d7d4a76e159a92a83fe930
CXCXHH <m15111642499@163.com>
2026-05-01 15:56:59 +0800

    test: 更新EEPROM测试逻辑并启用HEX输出

--- Bootloader_std/User/main.c
+++ Bootloader_std/User/main.c
@@ -2,7 +2,7 @@
 #include "bsp.h"
 
 uint8_t rbuff[256];
-uint8_t wbuff[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+uint8_t wbuff[16] = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
 
 int main(void)
 {
@@ -10,21 +10,12 @@ int main(void)
 
 	Bsp_Init();
 
-	for(i = 0;i < 8;i++)
-	{
-		AT24C02_WriteByte(i, 7-i);
-		Delay_ms(5);
-	}
+	AT24C02_WritePage(0, wbuff);
+	Delay_ms(5);
 
-	// for(i = 0;i < 16;i++)
-	// {
-	// 	AT24C02_WritePage(i*16, wbuff);
-	// 	Delay_ms(5);
-	// }
+	AT24C02_ReadData(0, rbuff, 16);
 
-	AT24C02_ReadData(0, rbuff, 256);
-
-	for(i = 0;i < 8;i++)
+	for(i = 0;i < 16;i++)
 	{
 		U1_printf("adrss%d=%x\r\n", i, rbuff[i]);
 	}
